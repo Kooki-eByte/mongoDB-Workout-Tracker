e997b60b039f54b2e0b4fc8869f8c02d3ad14bfe
Cristian Hornedo <officialcristianl@gmail.com>
2020-10-08 00:37:55 -0400

    Added the html route file, Inside it will listen for a /exercise route and bring the user to the exercise html page

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 
 module.exports = function (app) {
+  // Get method used to find all of the workouts in the Workout collection
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then((workoutData) => {
@@ -9,5 +10,9 @@ module.exports = function (app) {
       .catch((err) => {
         res.json(err);
       });
+  });
+
+  app.get("/exercise/:id", (req, res) => {
+    const workoutId = req.params.id;
   });
 };
