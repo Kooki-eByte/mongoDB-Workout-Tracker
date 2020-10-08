2a1b2065c9c8676200b9e4eb7d5edc2eec4faced
Cristian Hornedo <officialcristianl@gmail.com>
2020-10-08 14:35:30 -0400

    Add more validation to the models with default values as well as add a distance document

--- models/Workout.js
+++ models/Workout.js
@@ -25,22 +25,28 @@ const WorkoutSchema = new Schema({
         validate: [({ length }) => length >= 0, "Must be more than 0 minutes"],
       },
 
+      distance: {
+        type: Number,
+        validate: [({ length }) => length >= 0, "Must be more than 0 miles"],
+        default: null,
+      },
+
       weight: {
         type: Number,
-        required: true,
         validate: [({ length }) => length >= 0, "Must be more than 0 lbs"],
+        default: null,
       },
 
       reps: {
         type: Number,
-        required: true,
         validate: [({ length }) => length >= 0, "Must be more than 0 reps"],
+        default: null,
       },
 
       sets: {
         type: Number,
-        required: true,
         validate: [({ length }) => length >= 0, "Must be more than 0 sets"],
+        default: null,
       },
     },
   ],
