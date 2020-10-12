d7c91cfc43df24595d9a39b32303364f05fea8cf
Cristian Hornedo <officialcristianl@gmail.com>
2020-10-12 15:02:37 -0400

    Add stats api fetch and have the stats page show all of the information from the database

--- routes/api-route.js
+++ routes/api-route.js
@@ -12,6 +12,16 @@ module.exports = function (app) {
       });
   });
 
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((workoutData) => {
+        res.json(workoutData);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
   // This route is made to creating a new workout, insert it to the database
   app.put("/api/workouts/:id", (req, res) => {
     const workoutId = req.params.id;
