1d20e3fce21a8bbb50c1a81ded2a6a737a1bbd71
Cristian Hornedo <officialcristianl@gmail.com>
2020-10-12 16:00:30 -0400

    Adding the line of code for heroku env var to be found and let it be deployed

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/workoutTracker",
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   }
 );
 
